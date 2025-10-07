63f130e308a983b1cad99c0181cd50263964da16
syaban-khi <syabandikia2@gmail.com>
2025-10-07 11:03:13 +0700

    nambahin gambar default

--- database/migrations/2025_09_24_005412_add_gambar_to_produk_table.php
+++ database/migrations/2025_09_24_005412_add_gambar_to_produk_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('produk', function (Blueprint $table) {
-            $table->string('Gambar')->after('Stok')->default('produk/default.jpg');
+            $table->string('Gambar')->after('Stok')->default('produk/default.wepb');
         });
     }
 
